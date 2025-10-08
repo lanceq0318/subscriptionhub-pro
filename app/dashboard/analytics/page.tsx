59117ff823598e07083a7de5564fde9d550af3e1
lanceq0318 <lance.quismundo@kisamos-shipping.com>
2025-10-08 18:41:08 +0400

    Update page.tsx

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import StatCard from './StatCard';
-import SimpleBar from './SimpleBar';
+import StatCard from '@/app/components/StatCard';
+import SimpleBar from '@/app/components/SimpleBar';
 
 
 type Summary = {
